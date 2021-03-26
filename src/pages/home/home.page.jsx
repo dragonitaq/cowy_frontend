eeaabf18ca429265c3547a19a77f95bde91f32f3
dragonitaq <kevincharlk@gmail.com>
2021-03-26 22:59:10 +0800

    Implement home page with post title search feature

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import NavBar from '../../components/navBar/navBar.component';
+import PostPreview from '../../components/postPreview/postPreview.component';
 
 import * as S from './home.style';
 
 const Home = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [postsResult, setPostsResult] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:1337/posts')
+      .then((response) => {
+        console.log(response);
+        setPostsResult(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const findPosts = () => {
+    axios
+      .get(`http://localhost:1337/posts/?title_contains=${searchValue}`)
+      .then((response) => {
+        console.log(response);
+        setPostsResult(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <NavBar />
-      <S.HomeContainer>This is the home page.</S.HomeContainer>
+      <S.HomeContainer>
+        <S.Title>Welcome To COWY!</S.Title>
+        <S.Description>A simple blogging website.</S.Description>
+        <S.Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            findPosts();
+          }}
+        >
+          <S.Search
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <S.SearchButton>Search Title</S.SearchButton>
+        </S.Form>
+        <S.PostPreviewContainer>
+          {postsResult.map((post) => (
+            <PostPreview post={post} allowEdit={false} key={post.id} />
+          ))}
+        </S.PostPreviewContainer>
+      </S.HomeContainer>
     </div>
   );
 };
--- src/pages/home/home.style.jsx
+++ src/pages/home/home.style.jsx
@@ -3,7 +3,47 @@ import styled from 'styled-components';
 export const HomeContainer = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  background-color: grey; // TEST
+`;
+
+export const Title = styled.h1`
+  font-size: 4rem;
+`;
+
+export const Description = styled.h2`
+  font-size: 2.2rem;
+  margin-top: 1rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin: 2.5rem 0 1.5rem;
+`;
+
+export const Search = styled.input`
+  font-size: 2rem;
+  background-color: ${({ theme }) => theme.backgroundSecondary};
+  color: ${({ theme }) => theme.text};
+  border-color: ${({ theme }) => theme.primary};
+  margin-right: 0.5rem;
+`;
+
+export const SearchButton = styled.button`
+  cursor: pointer;
+  font-size: 1.5rem;
+  border: solid 1.5px ${({ theme }) => theme.secondary};
+  background-color: ${({ theme }) => theme.secondary};
+  border-radius: 15px;
+  text-align: center;
+  padding: 0.5rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PostPreviewContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(30rem, 1fr));
+  gap: 3rem;
+  padding: 2.5rem 5rem;
 `;
